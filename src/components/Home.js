093c397ce68e3be36ce4d0fca7a4512ea04d6e4e
ayan-goel <agoel320@gatech.edu>
2025-02-08 16:02:26 -0500

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Home = () => (
   <div className="home">
     <h1>Hi! I'm Ayan.</h1>
-    <p>I'm a Computer Science major at Georgia Tech, interested in software engineering, data science, and AI/ML! 
-      Explore my projects and learn more about me.</p>
+    <p>I'm a Computer Science major at Georgia Tech, interested in software engineering, data science, and AI/ML! </p>
   </div>
 );
 
